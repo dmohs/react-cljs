abc0bfa2e03f0dd6e344d30905d637c7fee14596
David Mohs <dgit@mohsinc.com>
2016-04-24 00:32:41 -0400

    cljs.env/*compiler* is an atom

--- src/dmohs/react/core.clj
+++ src/dmohs/react/core.clj
@@ -5,7 +5,7 @@
 
 (defmacro if-not-optimized [true-form false-form]
   (if (and env/*compiler*
-           (let [{:keys [optimizations]} (get env/*compiler* :options)]
+           (let [{:keys [optimizations]} (get @env/*compiler* :options)]
              (or (nil? optimizations) (= optimizations :none))))
     true-form
     false-form))
