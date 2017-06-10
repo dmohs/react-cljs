01d87469a5ecfd8f9cf91db3308c98715b87e2b9
David Mohs <dgit@mohsinc.com>
2017-06-10 19:36:57 -0400

    Bump React version

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (def source-paths ["src/main/cljs"])
 
-(defproject dmohs/react "1.2.0+15.5.4-0" ;; Also update README.md on version change.
+(defproject dmohs/react "1.2.0+15.5.4-1" ;; Also update README.md on version change.
   :description "A ClojureScript wrapper for React."
   :license "http://opensource.org/licenses/MIT"
   :url "https://github.com/dmohs/react-cljs"
   :dependencies [[cljsjs/create-react-class "15.5.3-0"]
-                 [cljsjs/react-dom "15.5.4-0"]] ; react-dom depends on react
+                 [cljsjs/react-dom "15.5.4-1"]] ; react-dom depends on react
   :source-paths ~source-paths
   :profiles {:ui-base
              {:plugins [[lein-cljsbuild "1.1.6"] [lein-figwheel "0.5.10"]]
